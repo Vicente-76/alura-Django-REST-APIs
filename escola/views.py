533ba10924b543bf4d252f160e51aa1e55d98509
Vicente Moraes <vicente.moraes@prefeitura.local>
2023-02-14 09:22:46 -0300

    added views to api

--- escola/views.py
+++ escola/views.py
@@ -1,9 +1,8 @@
-from rest_framework import viewsets
-from escola.models import Aluno, Curso
-from escola.serializer import AlunoSerializer, CursoSerializer
+from rest_framework import viewsets, generics
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
 
 
-# Create your views here.
 class AlunosViewSet(viewsets.ModelViewSet):
     """ Exibindo todos os alunos"""
     queryset = Aluno.objects.all()
@@ -11,5 +10,27 @@ class AlunosViewSet(viewsets.ModelViewSet):
 
 
 class CursosViewSet(viewsets.ModelViewSet):
+    """ Exibindo todos os cursos"""
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+
+
+class MatriculasViewSet(viewsets.ModelViewSet):
+    """ Exibindo todos as matriculas"""
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+
+
+class ListaMatriculasAlunos(generics.ListAPIView):
+    """ Exibindo todos as matriculas"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasAlunoSerializer
+
+class ListaAlunosMatriculados(generics.ListAPIView):
+    """ Listando alunos matriculados em um curso"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaAlunosMatriculadosSerializer
